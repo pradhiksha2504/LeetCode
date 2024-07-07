4cb7a56e7b141ad1f696204b14a6af0c9f6a42fe
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-07-07 18:05:12 +0530

    Time: 26 ms (95.57%), Space: 16.6 MB (28.82%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.py
+++ 2645-pass-the-pillow/2645-pass-the-pillow.py
@@ -6,28 +6,6 @@ class Solution:
                 count += 1
             return count
         else:
-            # count = 1
-            # t = 0
-            # while time>t:
-            #     curr += 1
-            #     t += 1
-            #     if curr == n:
-            #         count  = 1
-            #     if curr == 1:
-
-            # rem = time % n
-            # print(rem)
-            # count = 1
-            # quo = time // n
-            # if quo % 2 != 0 :
-            #     for i in range(rem+1):
-            #         n -= 1
-            #     return n
-            # else:
-            #     print("hi")
-            #     for i in range(rem):
-            #         count += 1
-            #     return count
             count = 1
             for i in range(1,time+1):
                 count = (count + 1)
@@ -36,6 +14,5 @@ class Solution:
                 if count == n:
                     count = -1*n
             return abs(count)
-        # rem = time % n
-        # return n - rem - 1
+
         
