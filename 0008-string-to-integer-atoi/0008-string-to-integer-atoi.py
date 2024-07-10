d63f0cd6894763a76d7a6e3afebf53614d105153
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-07-10 12:09:08 +0530

    Time: 39 ms (51.34%), Space: 16.5 MB (51.68%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -2,10 +2,8 @@ import sys
 class Solution:
     def myAtoi(self, s: str) -> int:
         s=s.lstrip()
-        # s = s.replace(" ","")
         if len(s)==0:
             return 0
-        print(s)
         a=""
         sign = 1
         if s[0] in ["-","+"]:
@@ -14,13 +12,11 @@ class Solution:
                 s = s.replace("-","",1)
             else:
                 s = s.replace("+","",1)
-        n = len(s)
-        for i in range(n):
+        for i in range(len(s)):
             if s[i].isdigit():
                 a+=s[i]
             else:
                 break
-        print(a)
         if len(a)!=0:
             num = sign*int(a)
             min_val = -2**31
