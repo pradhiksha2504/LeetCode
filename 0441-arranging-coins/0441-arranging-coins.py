d3ca0299803a5e314b233d8392308bf2fcf990cf
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-07-26 14:39:25 +0530

    Time: 621 ms (11.53%), Space: 16.3 MB (98.96%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -3,11 +3,11 @@ class Solution:
         row = 0
         i = 1
         while n >0:
+            if n < i:
+                return row
             n -= i
             i +=  1
             row += 1
-        if n < 0:
-            return row-1
         return row
 
         
