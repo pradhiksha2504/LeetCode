b8ecc4eecc3fb08b9508a39ac1ce746e645d28ea
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-07-26 14:41:55 +0530

    Time: 487 ms (28.54%), Space: 16.4 MB (58.44%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,13 +1,11 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        row = 0
         i = 1
         while n >0:
             if n < i:
-                return row
+                return i-1
             n -= i
             i +=  1
-            row += 1
-        return row
+        return i -1
 
         
