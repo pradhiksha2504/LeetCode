2d4871919823ce84786692eac5e2747028840688
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-07-31 13:57:49 +0530

    Time: 40 ms (31.28%), Space: 16.6 MB (41.94%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -4,15 +4,15 @@ class Solution:
             return False
         while n%2 == 0:
             n //=2
-        try:
-            while n!= 1:
-                if n % 3 == 0:
-                    n //= 3
-                elif n % 5 == 0:
-                    n //= 5
-                else:
-                    break
-        except:
-            return False
+        # try:
+        while n!= 1:
+            if n % 3 == 0:
+                n //= 3
+            elif n % 5 == 0:
+                n //= 5
+            else:
+                break
+        # except:
+            # return False
         if n == 1:
             return True
