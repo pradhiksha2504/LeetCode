32019ab115dd084b7d6282beb95bc63b8602b341
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-06 14:33:41 +0530

    Time: 259 ms (10.02%), Space: 17.8 MB (8.11%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        hm = {}
+        for i in word:
+            if i in hm:
+                hm[i] += 1
+            else:
+                hm[i] = 1
+        hm = {k: v for k, v in sorted(hm.items(), key=lambda item: item[1],reverse = True)}
+        count = 0
+        char= 0
+        for j in hm.values():
+            print(j)
+            count +=j * ((char // 8)+1)
+            char += 1
+        return count
+        
