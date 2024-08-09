bde4c2402d3818128e2dd2c900f8542d4cb72a91
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-09 15:24:40 +0530

    Time: 48 ms (14.36%), Space: 16.7 MB (22.56%) - LeetHub

---
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.py
@@ -0,0 +1,69 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        if rows < 3 or cols < 3:
+            return 0
+        global count
+        count = 0
+        
+        def search(r, c):
+            print("r c: ", r,c)
+            global count
+            matrix = []
+            dup = set()
+            for i in range(r,r+3):
+                row = []
+                for j in range(c,c+3):
+                    ele = grid[i][j]
+                    if ele in range(1,10):
+                        row.append(grid[i][j])
+                        dup.add(grid[i][j])
+                if len(row) == 3:
+                    matrix.append(row)
+                else:
+                    pass
+            print("matrix: ", matrix)
+            if magic(matrix,dup):
+                count += 1
+                print("count: ", count)
+        def magic(matrix, dup):
+            if len(dup)!= 9:
+                return False
+            d1, d2 = 0, 0
+            rowSum, colSum = 0,0
+            for i in matrix:
+                x = sum(i)
+                if x == rowSum or rowSum == 0:
+                    rowSum = x
+                else:
+                    return False
+            # print("rowsum: ", rowSum)
+            ind = 0
+            while ind < 3:
+                temp = 0
+                for i in range(3):
+                    temp += matrix[i][ind]
+                if colSum == 0 or temp == colSum:
+                    colSum = temp   
+                ind += 1
+            print("ROW AND COL SUM: ", rowSum, colSum)
+            r, c = 0, 0
+            r1,c1 = 2,0
+            while r < 3 and c < 3 and r1 >= 0 and c1 <3 :
+                d1 += matrix[r][c]
+                d2 += matrix[r1][c1]
+                r += 1
+                c += 1
+                r1 -=1
+                c1 += 1
+            print("d1, d2: ", d1, d2)
+            return d1 == d2 == rowSum == colSum
+
+        for i in range(rows):
+            for j in range(cols):
+                if i+3 <= rows and j+3<=cols:
+                    print("hi")
+                    search(i,j)
+
+        # print(count)
+        return count
