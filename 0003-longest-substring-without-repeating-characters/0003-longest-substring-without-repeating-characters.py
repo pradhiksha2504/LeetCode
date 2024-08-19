f3ca427df4a8bf1daa607cbdfcab12df80ce6931
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-19 11:50:00 +0530

    Time: 23 ms (96.55%), Space: 12.6 MB (31.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,8 +3,6 @@ class Solution(object):
         if len(s)==0:
             return 0
         b = s[0]
-        # ans = [b]
-        # mlen = len(ans[0])
         mlen = len(b)
         for i in range(1,len(s)):
             if s[i] in b:
@@ -14,6 +12,5 @@ class Solution(object):
             else:
                 b+=s[i]
             if len(b)>mlen:
-                # ans=[b]
                 mlen=len(b)
         return mlen
