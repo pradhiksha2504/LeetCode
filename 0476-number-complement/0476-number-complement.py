ae1d27c4db76edc81ce6ab08cfccbb440250a336
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-22 09:57:00 +0530

    Time: 30 ms (84.59%), Space: 16.5 MB (14.64%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,10 +1,12 @@
 class Solution:
     def findComplement(self, num: int) -> int:
         binary = format(num, 'b')
-        binary = str(binary)
-        binary = binary.replace("1","-")
-        binary = binary.replace("0","+")
-        binary = binary.replace("-","0")
-        binary = binary.replace("+","1")
+        binary = list(str(binary))
+        for i in range(len(binary)):
+            if binary[i] == "1":
+                binary[i] = "0"
+            else:
+                binary[i] = "1"
+        binary= "".join(map(str, binary))
 
         return int(binary,2)
