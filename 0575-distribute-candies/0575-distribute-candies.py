447115c3470955d0c4258686cd78e2545648f145
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-22 14:08:46 +0530

    Time: 639 ms (10.58%), Space: 18.5 MB (86.74%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -2,16 +2,13 @@ class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         num = len(candyType) // 2
         f = {}
-
         for i in candyType:
             if i in f:
                 f[i] += 1
             else:
                 f[i] = 1
-        
         types = f.keys()
-        if len(types) >= num:
+        n = len(types)
+        if n >= num:
             return num
-        return len(types)
-
-        
+        return n
