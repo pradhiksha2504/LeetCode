ca1470107955a15d62d181d79edb5187ece1013e
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-24 10:53:06 +0530

    Time: 90 ms (23.61%), Space: 16.8 MB (93.18%) - LeetHub

--- 0749-shortest-completing-word/0749-shortest-completing-word.py
+++ 0749-shortest-completing-word/0749-shortest-completing-word.py
@@ -8,26 +8,19 @@ class Solution:
                     f[i] += 1
                 else:
                     f[i] = 1
-        # print(f)
         ans = []
         length = len(f.keys())
-        # print("k: ",k)
         ind = 0
         for i in words:
             count = Counter(i)
-            # print(count)
             for k, v in f.items():
                 if k in count:
                     if count[k] >= v:
                         ind += 1
-                        # print("hi")
                         continue
                     else:
                         break
-            # print("ind: ", ind)
             if ind == length:
                 ans.append(i)
             ind = 0
-        # print(ans)
         return sorted(ans, key=lambda x:len(x))[0]
-        return ""
