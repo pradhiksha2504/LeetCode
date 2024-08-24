c9bb65f1a7bb0ab9203d513688c7f2453408f817
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2024-08-24 11:32:09 +0530

    Time: 92 ms (20.04%), Space: 16.9 MB (25.39%) - LeetHub

--- 0749-shortest-completing-word/0749-shortest-completing-word.py
+++ 0749-shortest-completing-word/0749-shortest-completing-word.py
@@ -1,20 +1,15 @@
 class Solution:
     def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
         licensePlate = licensePlate.lower()
-        # print(licensePlate)
         f = Counter(licensePlate)
-        # print(f)
         ans = []
         ind, x = 0, 0
         for i in words:
             count = Counter(i)
-            # print(count)
             for k, v in f.items():
                 if k.isalpha():
                     ind += 1
-                    # print("k: ",k)
                     if k in count:
-                        # print("k count: ", k)
                         if count[k] >= v:
                             x += 1
                         else:
@@ -22,5 +17,4 @@ class Solution:
             if ind == x:
                 ans.append(i)
             ind, x = 0,0
-        # print("ans: ", ans)
         return sorted(ans, key=lambda x:len(x))[0] if len(ans) > 0 else ""
