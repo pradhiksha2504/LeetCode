1740d7e0a30c4a951e1bfef799aabdc34caa28b9
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2025-01-29 00:42:52 +0530

    Time: 0 ms (100%), Space: 17.7 MB (35.28%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        key = ["qwertyuiop","asdfghjkl","zxcvbnm"]
+        res = []
+        for i in words:
+            s=i.lower()
+            f = {}
+            for j in s:
+                if j in f:
+                    continue
+                if j in "qwertyuiop" :
+                    f[j]=0
+                elif j in "asdfghjkl":
+                    f[j]=1
+                else:
+                    f[j]=2
+            if len(set(f.values())) == 1:
+                res.append(i)
+        return res
+
+                
+        
