1db600d127aecdb8739a75f94832f00003c60d58
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2025-03-13 14:22:22 +0530

    Time: 1624 ms (57.5%), Space: 35.3 MB (8.56%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
@@ -0,0 +1,32 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        primes = []
+
+        sieve = [True] * (right+1)
+        sieve[0] = sieve[1] = False
+
+        for i in range(2, int(right**0.5)+1):
+            if sieve[i]:
+                for j in range(i*i, right+1, i):
+                    sieve[j] = False
+
+        primes = [i for i in range(left,right+1) if sieve[i]]
+
+        if len(primes) < 2:
+            return [-1,-1]
+        if len(primes) == 2:
+            return primes
+        
+        # print(primes)
+        
+        minDiff = 2
+        ans = []
+        for i in range(len(primes)-1):
+            diff = primes[i+1]-primes[i]
+            if diff <= minDiff:
+                minDiff = diff
+                ans.append([primes[i], primes[i+1]])
+                return [primes[i], primes[i+1]]
+
+        return ans
+        
