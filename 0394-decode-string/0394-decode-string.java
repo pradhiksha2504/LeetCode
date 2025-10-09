26893070cfae9f315e48a3d3f495a7f0de5f695d
Pradhiksha K.S <140157237+pradhiksha2504@users.noreply.github.com>
2025-10-09 13:58:38 +0530

    Time: 1 ms (68%), Space: 41.7 MB (56.01%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -3,13 +3,12 @@ class Solution {
         Stack<String> stack = new Stack<>();
         for(int i = 0;i<s.length();i++) {
 			Character c = s.charAt(i);
-			String current = "";
 			if(c == ']') {
 				StringBuilder word = new StringBuilder();
 				while(!stack.isEmpty() && stack.peek().charAt(0) != '[') 
 				{
 					String curr = stack.pop();
-					if(!curr.equals(current) && Character.isAlphabetic(curr.charAt(0))) 
+					if(Character.isAlphabetic(curr.charAt(0))) 
 					{
 						word.insert(0,curr);
 					}
@@ -22,28 +21,19 @@ class Solution {
 				StringBuilder num = new StringBuilder();
 				while(!stack.isEmpty() && Character.isDigit(stack.peek().charAt(0))) 
 				{
-					num.append(stack.pop());
+					num.insert(0,stack.pop());
 				}
-				num.reverse();
-				
-				System.out.println(word + " " + num);
 				int count = Integer.parseInt(num.toString());
 				String x = word.toString();
 				for(int j = 0; j < count-1;j++) {
 					word.append(x);
 				}
-				System.out.println("after repeat: "+word);
 				stack.add(word.toString());
-				System.out.println("Stack: "+stack);
-				current = word.toString();
-				System.out.println("current: "+current);
-				
 			}
 			else {
 				stack.add(c.toString());
 			}
 		}
-		System.out.println("final stack: "+stack);
 
 		StringBuilder result = new StringBuilder();;
 		while(!stack.isEmpty()) {
